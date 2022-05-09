c217db1423dafe7919639d86ea8da8c236024dc5
ahmed tolan <Ahmed.Tolan@sh-eng.menofia.edu.eg>
2022-05-09 21:41:06 +0200

    Splitscreen init - No improvements

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { SplitScreen } from "./SplitScreen";
+
+export const LeftHandComponent = () => {
+  return <h1 style={{ backgroundColor: "red" }}>Left!</h1>;
+};
+export const RightHandComponent = () => {
+  return <h1 style={{ backgroundColor: "green" }}>Right!</h1>;
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <SplitScreen left={LeftHandComponent} right={RightHandComponent} />;
 }
 
 export default App;
