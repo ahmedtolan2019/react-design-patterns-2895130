28dc28fa1e84b072d14dafe170fa193b60f75fd2
ahmed tolan <Ahmed.Tolan@sh-eng.menofia.edu.eg>
2022-05-09 21:48:04 +0200

    Improve SplitScreen

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@ import logo from "./logo.svg";
 import "./App.css";
 import { SplitScreen } from "./SplitScreen";
 
-export const LeftHandComponent = () => {
-  return <h1 style={{ backgroundColor: "red" }}>Left!</h1>;
+export const LeftHandComponent = ({ name }) => {
+  return <h1 style={{ backgroundColor: "red" }}>{name}</h1>;
 };
-export const RightHandComponent = () => {
-  return <h1 style={{ backgroundColor: "green" }}>Right!</h1>;
+export const RightHandComponent = ({ message }) => {
+  return <h1 style={{ backgroundColor: "green" }}>{message}</h1>;
 };
 
 function App() {
-  return <SplitScreen left={LeftHandComponent} right={RightHandComponent} />;
+  return (
+    <SplitScreen leftWeight={1} rightWeight={3}>
+      <LeftHandComponent name="Left" />
+      <RightHandComponent message="messgae!!" />
+    </SplitScreen>
+  );
 }
 
 export default App;
