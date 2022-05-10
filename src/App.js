4ac88d7c675a7b53ef8646dc97a396ce696ac43b
ahmed tolan <Ahmed.Tolan@sh-eng.menofia.edu.eg>
2022-05-10 14:25:34 +0200

    Container patterns - Add General User loader container takes user id

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import { CurrentUserLoader } from './CurrentUserLoader';
-import { UserInfo } from './UserInfo';
+import { CurrentUserLoader } from "./CurrentUserLoader";
+import { UserInfo } from "./UserInfo";
+import { UserLoader } from "./UserLoader";
 
 function App() {
-	return (
-		<CurrentUserLoader>
-			<UserInfo />
-		</CurrentUserLoader>
-	);
+  return (
+    <>
+      <CurrentUserLoader>
+        <UserInfo />
+      </CurrentUserLoader>
+      <UserLoader userId={"234"}>
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId={"123"}>
+        <UserInfo />
+      </UserLoader>
+      <UserLoader userId={"345"}>
+        <UserInfo />
+      </UserLoader>
+    </>
+  );
 }
 
 export default App;
--- src/UserLoader.js
+++ src/UserLoader.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 
-export const CurrentUserLoader = ({ children }) => {
+export const UserLoader = ({ userId, children }) => {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
     (async () => {
-      const response = await axios.get("http://localhost:8080/current-user");
+      const response = await axios.get(`http://localhost:8080/users/${userId}`);
       setUser(response.data);
     })();
-  }, []);
+  }, [userId]);
 
   return (
     <>
